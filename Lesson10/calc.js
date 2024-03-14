f5239a258823efe30dd963e013ea0d76e0e54832
Fuad Pashayev <fuad.pashayev12@gmail.com>
2024-03-14 12:22:43 +0400

    added last lessons

---
+++ Lesson10/calc.js
@@ -0,0 +1,109 @@
+/*
+Dat Types:
+
+- string
+- number
+- boolean
+
+
+array - list (type: object)
+
+var list = [1, 2, 3, 4, 5, 6];
+
+- Deyer almaq ucun
+list[-index-] -> list[2] -> 3
+
+list[2] = 'salam' -> 2ci indexdeki deyer 'salam' deyerine cevrilir (mutation)
+
+
+var numbers = [1, 3, 5];
+
+Elave etmek
+
+numbers.push(7); -> sonuna
+numbers.unshift(-3434); -> evveline
+
+
+Silmek
+numbers.shift() -> evvelden silir
+numbers.pop() -> sonundan silir
+
+
+
+
+
+*/
+
+// var operatorList = "+,-,*,/,%";
+
+// var operatorList = ["+", "-", "*", "/", "%"];
+
+// var a = +prompt("Birinci ededi daxil edin");
+// var operator = prompt("Operator");
+
+// if (!operatorList.includes(operator)) {
+//   alert("Operator yanlishdir");
+//   operator = prompt("Operator");
+// }
+
+// var b = +prompt("Ikinci ededi daxil edin");
+
+// var result;
+
+/*
+==
+===
+!=
+!==
+! -> not
+
+Boolean deyerler:
+true / false
+
+Booly deyerler:
+
+butun deyerler (truthy / falsy)
+!0 -> falsy
+'' -> falsy
+1 -> truthy
+'sdfsdf' -> truthy
+
+! - not - deyil
+
+!(4 < 5)
+
+
+
+!true = false
+!false = true
+0 = 
+
+*/
+
+// if (operator === "+") {
+//   result = a + b;
+// } else if (operator === "-") {
+//   result = a - b;
+// } else if (operator === "*") {
+//   result = a * b;
+// } else if (operator === "/") {
+//   result = a / b;
+// } else if (operator === "%") {
+//   result = a % b;
+// } else {
+//   result = "Operator yanlishdir!";
+// }
+
+// var resultText = `Netice: ${result}`;
+
+// console.log(resultText);
+
+// var okay = 4 < 5;
+
+// 'salam'
+
+// if (!"salam") {
+//   console.log("a");
+// } else {
+//   console.log("b");
+// }
---
+++ Lesson10/index.js
@@ -0,0 +1,16 @@
+// if(true){
+//     console.log('a')
+// }else{
+//     console.log('b')
+// }
+
+// ternary - if/else operatorunun qisa formasi
+var a = 5;
+
+var result = a < 5 ? "a 5den kicikdir" : "a 5den kicik deyil";
+
+console.log(result);
+
+var number = 3;
+
+var result = 3 + 5 * 2 + 2;
